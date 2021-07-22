8a4f10604069ce3d4e520a173009d10dfbca995d
hk0305 <hyounkeun2009@gmail.com>
2021-07-22 21:00:46 +0900

    과제 2-1까지 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    void contains() {
+
+        // 과제 2-1
+        assertThat(numbers).hasSize(4);
+
+        // TODO 과제 2-2
+
+    }
+
+
+
+
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,39 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
+
     @Test
     void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+
+        // ## 과제 1 ##
+        // 과제 1-1
+        //String[] splitStrNum = "1,2".split(",");
+        //assertThat(splitStrNum).contains("1", "2", splitStrNum[0], splitStrNum[1]);
+
+        // 과제 1-2
+        //String[] splitStrNum1 = "1".split(",");
+        //assertThat(splitStrNum1).containsExactly("1");
+
+        // 과제 1-3
+        //String strNum = "(1,2)".substring(1, 4);
+        //assertThat(strNum).isEqualTo("1,2");
+
+        // 과제 1-4
+//        assertThatThrownBy(() -> {
+//            String strNum1 = "abc";
+//            strNum1.charAt(3);
+//        }).isInstanceOf(IndexOutOfBoundsException.class)
+//                .hasMessage("String index out of range: 3");
+
+        // ## 과제 2 ##
+
     }
+
+
 }
